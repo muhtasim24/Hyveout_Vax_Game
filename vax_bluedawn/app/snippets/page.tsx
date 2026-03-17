d4821494cf718f54cdaa24d5f41e389d3da97247
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-03-17 17:28:13 -0400

    draft of snippets page

--- vax_bluedawn/app/snippets/page.tsx
+++ vax_bluedawn/app/snippets/page.tsx
@@ -1,11 +1,33 @@
+import SnippetVid from "@/components/snippetVid";
+import Image from "next/image";
 import Link from "next/link";
 
 
 export default function Snippets() {
+
+    const img: string = "/lock.png";
+
     return (
-        <div>
-            <h1>SNIPPET PAGE</h1>
-            <Link href="/" className="text-white">GO BACK</Link>
+        <div className="flex flex-col justify-center items-center gap-8">
+            <div className="">
+                <Image 
+                    src={"/logo.png"} 
+                    alt="blue dawn logo"
+                    width = {200}
+                    height = {200}
+                    className="w-150 h-55"
+                />
+            </div>
+
+            <div className = "grid grid-cols-2 gap-8">
+                <SnippetVid video = {img}/>
+                <SnippetVid video = {img}/>
+                <SnippetVid video = {img}/>
+                <SnippetVid video = {img}/>
+
+            </div>
+
+            <Link href="/" className="text-white text-3xl">GO BACK</Link>
         </div>
     )
 }
--- vax_bluedawn/components/snippetVid.tsx
+++ vax_bluedawn/components/snippetVid.tsx
@@ -1,7 +1,20 @@
 'use client';
 
-export default function SnippetVid() {
+import Image from "next/image";
+
+type Props = {
+    video: string;
+}
+
+
+export default function SnippetVid( {video} : Props) {
     return (
-        <h1>Snipet</h1>
+        <Image 
+            src = {video}
+            alt = {"videos"}
+            width = {100}
+            height = {100}
+            className = "w-40 border border-blue-700 border-6 bg-black"
+        />
     )
 }
