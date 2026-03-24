c5adf28f28bf11e99657af95ebe1856309c2b2f5
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-03-24 16:27:38 -0400

    turn bird red when clicked too

--- vax_bluedawn/components/birdBtn.tsx
+++ vax_bluedawn/components/birdBtn.tsx
@@ -1,25 +1,50 @@
 'use client';
 
+import { useState } from "react";
 import Image from "next/image";
 
-export default function BirdBtn() {
-    return (
-        <div className="group relative inline-block cursor-pointer">
-            <Image 
-                src="/assets/whitebird.png"
-                alt="white bird"
-                width={100}
-                height={100}
-                className="block group-hover:hidden group-active:hidden"
-            />
 
-            <Image 
-                src="/assets/redbird.png"
-                alt="red bird"
-                width={100}
-                height={100}
-                className="hidden group-hover:block group-active:block"
-            />
-        </div>
-    )
+export default function BirdBtn() {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true); // stays red after click
+  };
+
+  return (
+    <div
+      className="relative inline-block cursor-pointer group"
+      onClick={handleClick}
+    >
+      {/* White bird: show if not hovered and not clicked */}
+      {!clicked && (
+        <Image
+            src="/assets/whitebird.png"
+            alt="white bird"
+            width={100}
+            height={100}
+            className="block group-hover:hidden"
+        />
+      )}
+
+      {/* Red bird: show on hover OR clicked */}
+      {(clicked) && (
+        <Image
+            src="/assets/redbird.png"
+            alt="red bird"
+            width={100}
+            height={100}
+        />
+      )}
+      {!clicked && (
+        <Image
+            src="/assets/redbird.png"
+            alt="red bird"
+            width={100}
+            height={100}
+            className="hidden group-hover:block"
+        />
+      )}
+    </div>
+  );
 }
