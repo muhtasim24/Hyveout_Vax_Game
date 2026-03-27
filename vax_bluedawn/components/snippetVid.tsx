88ddb0525a657f308ffc20eeb857b71ab2022f80
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-03-27 00:11:22 -0400

    snippets page edits

--- vax_bluedawn/components/snippetVid.tsx
+++ vax_bluedawn/components/snippetVid.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export default function SnippetVid( {video, locked} : Props) {
     return (
-        <div className="w-full max-w-[250px] aspect-video border-4 border-blue-700 bg-black flex justify-center items-center">
+        <div className="w-full max-w-[250px] aspect-video border-4 border-blue-700 bg-white flex justify-center items-center md:max-w-[300px]">
             {locked ? (
                 <Image 
                     src = {video}
@@ -18,7 +18,7 @@ export default function SnippetVid( {video, locked} : Props) {
                     width = {100}
                     height = {100}
                     className = 
-                    "p-4 md:p-2"
+                    "p-6 md:p-4"
                 />
             ) : (
                 <video
