0fcf582442676a83138e6da0e7aeacd1bb1f9125
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-03-27 00:04:19 -0400

    blue dawn page responsive

--- vax_bluedawn/app/bluedawn/page.tsx
+++ vax_bluedawn/app/bluedawn/page.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 
 export default function BlueDawn() {
     return (
-        <div className="h-[80vh] flex flex-col justify-between items-center py-2">
+        <div className="flex flex-col justify-center items-center pt-16 md:pt-20">
           
                 <div className="flex flex-col items-center gap-6">
                     <h1 className="text-white font-bold text-2xl md:text-3xl text-center max-w-[70vw] md:max-w-[500px]">A blue dawn arises between the night sky and the sunrise.</h1>
                     <h1 className="text-white font-bold text-2xl md:text-3xl text-center max-w-[80vw] md:max-w-[680px]">Things are uncertain at this moment, but with time everything comes into focus</h1>
                 </div>
 
-                <Link href="/" className="text-2xl cursor-pointer text-white hover:text-red-500 active:text-red-700 md:text-4xl">
+                <Link href="/" className="pt-10">
                     <BirdBtn/>
                 </Link>
             
