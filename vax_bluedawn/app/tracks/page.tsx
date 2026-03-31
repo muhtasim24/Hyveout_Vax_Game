124d3671b14329ac711983fbcb41f248772fdf8a
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-03-31 15:28:39 -0400

    test img size

--- vax_bluedawn/app/tracks/page.tsx
+++ vax_bluedawn/app/tracks/page.tsx
@@ -17,9 +17,9 @@ export default async function Tracks() {
     
     console.log(trackList);
     return (
-        <div className="w-full h-full flex flex-col justify-between items-center py-2 px-4">
+        <div className="w-full h-full flex flex-col justify-start items-center pt-1 gap-2 px-4 md:pt-4 md:gap-4">
 
-            <div className="w-full max-w-[900px] flex flex-col justify-center items-center md:flex-row md:justify-center md:items-center md:gap-10">
+            <div className="w-full max-w-[900px] flex flex-col items-center md:flex-row md:justify-center md:items-center md:gap-10">
 
                 {/* LEFT SIDE DESKTOP */}
                 <div className="hidden md:flex flex-col flex-1">
@@ -29,7 +29,7 @@ export default async function Tracks() {
                 <div className="flex flex-col items-center flex-1 gap-2">
                     <h1 className={`${player2.className} text-blue-500 font-bold text-xl md:text-2xl lg:text-3xl md:-mt-2 [text-shadow:_1px_0_0_white,_-1px_0_0_white,_0_1px_0_white,_0_-1px_0_white] `}>4/24/26</h1>
                     
-                    <div className="relative aspect-square w-[15vh] md:w-[clamp(180px,20vw,270px)]">
+                    <div className="relative aspect-square w-[15vh] md:w-[clamp(150px,20vw,270px)]">
                         <Image 
                             src = {"/assets/lock.png"}
                             alt = "cover art"
@@ -45,7 +45,7 @@ export default async function Tracks() {
                 </div>
 
                 {/* MOBILE ALWAYS */}
-                <div className="flex flex-col md:hidden w-full max-h-[30vh] overflow-y-auto ">
+                <div className="flex flex-col md:hidden w-full ">
                     <TrackSelect trackList={trackList} />
                 </div>
 
