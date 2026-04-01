ac2a6997cfa47af6edb36befec6b8a0354cf59ef
Muhtasim Mushfiq <muhtasim01@gmail.com>
2026-03-31 23:42:02 -0400

    lowering snippets page

--- vax_bluedawn/app/bluedawn/page.tsx
+++ vax_bluedawn/app/bluedawn/page.tsx
@@ -9,7 +9,7 @@ export default function BlueDawn() {
           
                 <div className="flex flex-col items-center gap-6">
                     <h1 className="text-white font-bold text-2xl md:text-3xl text-center max-w-[70vw] md:max-w-[500px]">A blue dawn arises between the night sky and the sunrise.</h1>
-                    <h1 className="text-white font-bold text-2xl md:text-3xl text-center max-w-[80vw] md:max-w-[680px]">Things are uncertain at this moment, but with time everything comes into focus</h1>
+                    <h1 className="text-white font-bold text-2xl md:text-3xl text-center max-w-[80vw] md:max-w-[680px]">Things are uncertain at this moment, but with time everything comes into focus.</h1>
                 </div>
 
                 <Link href="/" className="pt-10">
